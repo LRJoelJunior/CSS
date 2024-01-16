96bf4b3d4a10266f6e8ebc2c8058683a217cded8
Joël Junior Leroy Rodriguez <leroyrjoeljr@gmail.com>
2024-01-16 20:47:10 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,11 @@
+
+
+function btn1() {
+    document.body.style.backgroundColor = "red";
+  }
+  function btn2() {
+    document.body.style.backgroundColor = "orange";
+  }
+  function btn3() {
+    document.body.style.backgroundColor = "yellow";
+  }
